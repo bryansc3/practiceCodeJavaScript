dade3b308041b979c5a147584a85174a2cad7c2d
Bryan <bryan.soca03@gmail.com>
2023-09-02 22:16:03 -0500

    Create CaesarsCipher.js

---
+++ CaesarsCipher.js
@@ -0,0 +1,9 @@
+function rot13(str) {
+  //A:65, Z:90
+  return str.split("").map(m=>!/\W/.test(m)?String.fromCodePoint(m.charCodeAt()+13>90?
+                                                                    m.charCodeAt()-13:
+                                                                    m.charCodeAt()+13)
+                                                                :m).join("");
+}
+
+console.log(rot13("SERR PBQR PNZC")); //This return: FREE CODE CAMP
