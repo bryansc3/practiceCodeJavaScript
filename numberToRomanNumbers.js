5f83238cd795b106b90d1e004ed0dab4404cf206
Bryan <bryan.soca03@gmail.com>
2023-09-02 20:13:49 -0500

    Update numberToRomanNumber.js

--- numberToRomanNumbers.js
+++ numberToRomanNumbers.js
@@ -1,26 +1,27 @@
 function convertToRoman(num) {
-  const listNum={1: "I", 4:"IV", 5:"V", 9:"IX", 10:"X", 
-                40:"XL", 50:"L", 90:"XC", 100:"C",
-                400:"CD", 500:"D", 900:"CM", 1000:"M"};
+  const listNum={1: "I", 4:"IV", 5:"V", 9:"IX", 
+                 10:"X", 40:"XL", 50:"L", 90:"XC", 
+                 100:"C", 400:"CD", 500:"D", 900:"CM", 
+                 1000:"M"};
 
-  let num0=Math.floor(num/1000);
-  let num1=Math.floor(num%1000/100)*100;
-  let num2=Math.floor(num%100/10)*10;
-  let num3=num%10;
-  let arr=[listNum[1000].repeat(num0),
+  const arr=[Math.floor(num/1000),
+             Math.floor(num%1000/100),
+             Math.floor(num%100/10),
+             num%10];
   
-        num1<400?listNum[100].repeat(num1/100)
-        :num1==400||num1==500?listNum[num1]:num1<900?
-        listNum[500]+listNum[100].repeat((num1-500)/100):listNum[900],
-
-        num2<40?listNum[10].repeat(num2/10)
-        :num2==40||num2==50?listNum[num2]:num2<90?
-        listNum[50]+listNum[10].repeat((num2-50)/10):listNum[90],
-
-        num3<4?listNum[1].repeat(num3)
-        :num3==4||num3==5?listNum[num3]:num3<9?
-        listNum[5]+listNum[1].repeat(num3-5):listNum[9]]
- return arr.join("");
+  const romanNumber=arr.map((m,ind)=>
+                      {let a=Math.pow(10,3-ind); 
+                          return ind!=0?
+                                    m<4?
+                                      listNum[a].repeat(m):
+                                    m==4||m==5?
+                                      listNum[m*a]:
+                                    m<9?
+                                      listNum[5*a]+listNum[a].repeat((m-5)):
+                                    listNum[9*a]
+                                 :listNum[a].repeat(m)
+                      });
+  return romanNumber.join("");
 }
 
-console.log(convertToRoman(83));
+console.log(convertToRoman(3243));
